8ce4ba9fb3930a8fe88e2b7839309def571647ab
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-13 12:17:08 +0200

    Create testMergeSort

--- src/test/java/MergeSortTest.java
+++ src/test/java/MergeSortTest.java
@@ -1,2 +1,31 @@
-public class MergeSortTest {
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeSortTest {
+
+    private int[] createArrayToSort() {
+        int[] array = new int[50];
+        int randomNumber;
+        for (int i = 0; i < array.length; i++) {
+            randomNumber = new Random().nextInt(500);
+            array[i] = randomNumber;
+        }
+        return array;
+    }
+
+    @Test
+    void testMergeSort() {
+        int[] array = createArrayToSort();
+        int[] sortedArray = array.clone();
+
+        Arrays.sort(sortedArray);
+        MergeSort.sort(array);
+
+        assertEquals(Arrays.toString(sortedArray), Arrays.toString(array));
+    }
+
 }
