4fbc6d0667a6890d2707e7345e98f628e134f4c6
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-13 12:18:38 +0200

    Create mergeSort method

--- src/main/java/MergeSort.java
+++ src/main/java/MergeSort.java
@@ -6,4 +6,14 @@ public class MergeSort {
         tempArray = new int[array.length];
         mergeSort(array,0, array.length - 1);
     }
+
+    private static void mergeSort(int[] array, int left, int right) {
+
+        if (left < right) {
+            int middle = left + (right - left) / 2;
+            mergeSort(array, left, middle);
+            mergeSort(array, middle + 1, right);
+            merge(array, left, middle, right);
+        }
+    }
 }
