5f43a1848ccb42c207700ddb80b39de98d4a2b7b
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-13 12:18:24 +0200

    Create sort method

--- src/main/java/MergeSort.java
+++ src/main/java/MergeSort.java
@@ -1,2 +1,9 @@
 public class MergeSort {
+
+    private static int[] tempArray;
+
+    public static void sort(int[] array) {
+        tempArray = new int[array.length];
+        mergeSort(array,0, array.length - 1);
+    }
 }
